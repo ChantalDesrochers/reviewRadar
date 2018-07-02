e3a9fb13ab4708bd15fd5cb0d13ee0ce5dc23a1b
Jeff <jeff.tang@live.com>
2018-07-02 14:58:23 -0400

    added function to remove duplicate concept from single review

--- express/public/javascripts/integratedmeaningcloud.js
+++ express/public/javascripts/integratedmeaningcloud.js
@@ -81,7 +81,9 @@ const requester = (review, cbMC) => {
 };
 
 function iterateWithDelay(array) {
-  for (let i = 0; i < array.length; i++) {
+  // for (let i = 3; i < 4; i++) {
+  // for (let i = 1; i < 2; i++) {
+    for (let i = 0; i < array.length; i++) {
     // console.log(array[i])
     (function(i) {
       setTimeout(function() {
@@ -91,6 +93,12 @@ function iterateWithDelay(array) {
   }
 }
 
+checkForExisting = match => element => {
+  // console.log('inside checkforexisting element', element.content)
+  // console.log('inside checkforexisting', match)
+  return element.content == match;
+};
+
 parseMC = review => (error, response, body) => {
   review.concepts = [];
   let results = JSON.parse(body);
@@ -98,13 +106,19 @@ parseMC = review => (error, response, body) => {
     results.concept_list.forEach(function(item) {
       if (item.relevance > 0) {
         // threshold of relevance
-        review.concepts.push({
-          content: item.form,
-          position: review.id
-        });
+        // console.log('current review.concepts', review.concepts)
+        console.log("item form", item.form);
+        checkResult = review.concepts.findIndex(checkForExisting(item.form));
+        console.log(checkResult);
+        if (checkResult < 0) {
+          review.concepts.push({
+            content: item.form,
+            position: review.id
+          });
+        }
       }
     });
-    console.log(review);
+    console.log(JSON.stringify(inputReviewArray));
   } else {
     console.log("error occured", results.status);
   }
@@ -113,3 +127,186 @@ parseMC = review => (error, response, body) => {
 iterateWithDelay(inputReviewArray);
 
 // request(options, callback, api_key, urlencodedtext);
+
+outputJSON = [
+  {
+    id: 0,
+    rating: "4.0",
+    author: "Ksenja T.",
+    description:
+      "I tried Impact Kitchen when it first opened via Uber eats. They were still working out a few kinks, especially with regard to portion size, which I found to be too small.\n\nThey have since improved a lot, adding a lot of different menu items and to-go options. Angela runs a tight ship and this is now one of my go-to spots. The only downside is that it is fairly expensive.\n\nMy favorite items on the menu are the winter citrus salad with falafel add-on; chicken bone broth, thai butternut squash soup (delicious!) and the halo power bowl.\n\nThe collagen chocolate pudding is an amazing dessert for when those cravings kick in.",
+    datePublished: "2017-04-12",
+    label: "positive",
+    score: 0.0647312,
+    concepts: [
+      { content: "soup", position: 0 },
+      { content: "boat", position: 0 },
+      { content: "advertisement", position: 0 },
+      { content: "spot", position: 0 },
+      { content: "favorite", position: 0 },
+      { content: "winter", position: 0 },
+      { content: "citrus", position: 0 },
+      { content: "salad", position: 0 },
+      { content: "attention deficit disorder", position: 0 },
+      { content: "chicken", position: 0 },
+      { content: "bone", position: 0 },
+      { content: "Thai", position: 0 },
+      { content: "chocolate", position: 0 },
+      { content: "pudding", position: 0 },
+      { content: "dessert", position: 0 }
+    ]
+  },
+  {
+    id: 1,
+    rating: "5.0",
+    author: "Jacquie O.",
+    description:
+      'This place tastes "healthy" in the way that means "fresh" and not as a euphemism for "bland". Ordered a workplace dinner from here on hungrhub. Food arrived on time, of good quality, flavourful and pretty. Prices very reasonable and falafel is even moist!\n\nYum',
+    datePublished: "2017-11-29",
+    label: "positive",
+    score: 0.749361,
+    concepts: [
+      { content: "dinner", position: 1 },
+      { content: "dinner", position: 1 },
+      { content: "food", position: 1 }
+    ]
+  },
+  {
+    id: 2,
+    rating: "3.0",
+    author: "Ana C.",
+    description:
+      "Annoyed at the very slow slow service. \nFor the price and location I expect at least a bag and some cutlery.\nI had to ask for a bag, and they didn't bother putting in napkins and cutlery.\n\nI waiting 10 min for an iced americano when there was no one ahead of me. \n\nFor an experience that is set up to be streamline,\nthey need to stop hiring kids and speed things up asap.",
+    datePublished: "2017-10-06",
+    label: "negative",
+    score: -0.38222,
+    concepts: [
+      { content: "bag", position: 2 },
+      { content: "cutlery", position: 2 },
+      { content: "service", position: 2 },
+      { content: "location", position: 2 },
+      { content: "location", position: 2 },
+      { content: "kid", position: 2 },
+      { content: "speed", position: 2 }
+    ]
+  },
+  {
+    id: 3,
+    rating: "5.0",
+    author: "Matt B.",
+    description:
+      "Excellent taco stand with great food to go along with good prices and service.  I found about this place from an article on Blog.To.   I had the Baja fish haddock tacos and they were among the best tacos I had anywhere, and that includes those made in Texas or California.    \n\nThere is very limited seating and it is cash only.    But I am not going to let that cost them a five-star review by making Seven Lives out to be something that it is not.    I would strongly suggest this establishment if you are craving for tacos, enough said.",
+    datePublished: "2018-06-22",
+    label: "positive",
+    score: 0.165046,
+    concepts: [
+      { content: "taco", position: 3 },
+      { content: "food", position: 3 },
+      { content: "service", position: 3 },
+      { content: "place", position: 3 },
+      { content: "article", position: 3 },
+      { content: "article", position: 3 },
+      { content: "fish", position: 3 },
+      { content: "establishment", position: 3 }
+    ]
+  },
+  {
+    id: 4,
+    rating: "5.0",
+    author: "Aastha S.",
+    description:
+      "Went here during my first trip to Kensington, and found their tacos to be just pure deliciousness! I tried their daily special - blackened cod with mango/pineapple salsa, and for $7, it was the right proportion and flavours! The only down side is there is always a line-up and barely any space to eat indoors, so we ended up eating on the street. \n\nThe place offers a variety of vegetarian and meat options, and has their own take on the authentic Mexican tacos. Best to park nearby and walk here as part of your Kensington market loop. Also remember to bring cash and they don't accept credit cards! \n\nDefinitely coming back for some more another day!",
+    datePublished: "2018-06-10",
+    label: "positive",
+    score: 0.336056,
+    concepts: [
+      { content: "taco", position: 4 },
+      { content: "credit card", position: 4 },
+      { content: "special", position: 4 },
+      { content: "cod", position: 4 },
+      { content: "mango", position: 4 },
+      { content: "$", position: 4 },
+      { content: "side", position: 4 },
+      { content: "side", position: 4 },
+      { content: "team", position: 4 },
+      { content: "space", position: 4 },
+      { content: "space", position: 4 },
+      { content: "street", position: 4 },
+      { content: "place", position: 4 },
+      { content: "meat", position: 4 },
+      { content: "Mexican", position: 4 },
+      { content: "market", position: 4 },
+      { content: "trip", position: 4 }
+    ]
+  }
+];
+
+
+let noDupeJSON =
+[{"id":0,"rating":"4.0","author":"Ksenja T.","description":"I tried Impact Kitchen when it first opened via Uber eats. They were still working out a few kinks, especially with regard to portion size, which I found to be too small.\n\nThey have since improved a lot, adding a lot of different menu items and to-go options. Angela runs a tight ship and this is now one of my go-to spots. The only downside is that it is fairly expensive.\n\nMy favorite items on the menu are the winter citrus salad with falafel add-on; chicken bone broth, thai butternut squash soup (delicious!) and the halo power bowl.\n\nThe collagen chocolate pudding is an amazing dessert for when those cravings kick in.","datePublished":"2017-04-12","label":"positive","score":0.0647312,"concepts":[{"content":"soup","position":0},{"content":"boat","position":0},{"content":"advertisement","position":0},{"content":"spot","position":0},{"content":"favorite","position":0},{"content":"winter","position":0},{"content":"citrus","position":0},{"content":"salad","position":0},{"content":"attention deficit disorder","position":0},{"content":"chicken","position":0},{"content":"bone","position":0},{"content":"Thai","position":0},{"content":"chocolate","position":0},{"content":"pudding","position":0},{"content":"dessert","position":0}]},{"id":1,"rating":"5.0","author":"Jacquie O.","description":"This place tastes \"healthy\" in the way that means \"fresh\" and not as a euphemism for \"bland\". Ordered a workplace dinner from here on hungrhub. Food arrived on time, of good quality, flavourful and pretty. Prices very reasonable and falafel is even moist!\n\nYum","datePublished":"2017-11-29","label":"positive","score":0.749361,"concepts":[{"content":"dinner","position":1},{"content":"food","position":1}]},{"id":2,"rating":"3.0","author":"Ana C.","description":"Annoyed at the very slow slow service. \nFor the price and location I expect at least a bag and some cutlery.\nI had to ask for a bag, and they didn't bother putting in napkins and cutlery.\n\nI waiting 10 min for an iced americano when there was no one ahead of me. \n\nFor an experience that is set up to be streamline,\nthey need to stop hiring kids and speed things up asap.","datePublished":"2017-10-06","label":"negative","score":-0.38222,"concepts":[{"content":"bag","position":2},{"content":"cutlery","position":2},{"content":"service","position":2},{"content":"location","position":2},{"content":"kid","position":2},{"content":"speed","position":2}]},{"id":3,"rating":"5.0","author":"Matt B.","description":"Excellent taco stand with great food to go along with good prices and service.  I found about this place from an article on Blog.To.   I had the Baja fish haddock tacos and they were among the best tacos I had anywhere, and that includes those made in Texas or California.    \n\nThere is very limited seating and it is cash only.    But I am not going to let that cost them a five-star review by making Seven Lives out to be something that it is not.    I would strongly suggest this establishment if you are craving for tacos, enough said.","datePublished":"2018-06-22","label":"positive","score":0.165046,"concepts":[{"content":"taco","position":3},{"content":"food","position":3},{"content":"service","position":3},{"content":"place","position":3},{"content":"article","position":3},{"content":"fish","position":3},{"content":"establishment","position":3}]},{"id":4,"rating":"5.0","author":"Aastha S.","description":"Went here during my first trip to Kensington, and found their tacos to be just pure deliciousness! I tried their daily special - blackened cod with mango/pineapple salsa, and for $7, it was the right proportion and flavours! The only down side is there is always a line-up and barely any space to eat indoors, so we ended up eating on the street. \n\nThe place offers a variety of vegetarian and meat options, and has their own take on the authentic Mexican tacos. Best to park nearby and walk here as part of your Kensington market loop. Also remember to bring cash and they don't accept credit cards! \n\nDefinitely coming back for some more another day!","datePublished":"2018-06-10","label":"positive","score":0.336056,"concepts":[{"content":"taco","position":4},{"content":"credit card","position":4},{"content":"special","position":4},{"content":"cod","position":4},{"content":"mango","position":4},{"content":"$","position":4},{"content":"side","position":4},{"content":"team","position":4},{"content":"space","position":4},{"content":"street","position":4},{"content":"place","position":4},{"content":"meat","position":4},{"content":"Mexican","position":4},{"content":"market","position":4},{"content":"trip","position":4}]}]
+
+
+let allConcepts = [];
+
+// outputJSON.forEach(function(review) {
+//   review.concepts.forEach(function(concept) {
+//     allConcepts.push({
+//       concept: concept.content,
+//       reviews: review.id
+//     })
+//   })
+// })
+// console.log(allConcepts)
+
+undefinedvar = [
+  { concept: "soup", reviews: 0 },
+  { concept: "boat", reviews: 0 },
+  { concept: "advertisement", reviews: 0 },
+  { concept: "spot", reviews: 0 },
+  { concept: "favorite", reviews: 0 },
+  { concept: "winter", reviews: 0 },
+  { concept: "citrus", reviews: 0 },
+  { concept: "salad", reviews: 0 },
+  { concept: "attention deficit disorder", reviews: 0 },
+  { concept: "chicken", reviews: 0 },
+  { concept: "bone", reviews: 0 },
+  { concept: "Thai", reviews: 0 },
+  { concept: "chocolate", reviews: 0 },
+  { concept: "pudding", reviews: 0 },
+  { concept: "dessert", reviews: 0 },
+  { concept: "dinner", reviews: 1 },
+  { concept: "dinner", reviews: 1 },
+  { concept: "food", reviews: 1 },
+  { concept: "bag", reviews: 2 },
+  { concept: "cutlery", reviews: 2 },
+  { concept: "service", reviews: 2 },
+  { concept: "location", reviews: 2 },
+  { concept: "location", reviews: 2 },
+  { concept: "kid", reviews: 2 },
+  { concept: "speed", reviews: 2 },
+  { concept: "taco", reviews: 3 },
+  { concept: "food", reviews: 3 },
+  { concept: "service", reviews: 3 },
+  { concept: "place", reviews: 3 },
+  { concept: "article", reviews: 3 },
+  { concept: "article", reviews: 3 },
+  { concept: "fish", reviews: 3 },
+  { concept: "establishment", reviews: 3 },
+  { concept: "taco", reviews: 4 },
+  { concept: "credit card", reviews: 4 },
+  { concept: "special", reviews: 4 },
+  { concept: "cod", reviews: 4 },
+  { concept: "mango", reviews: 4 },
+  { concept: "$", reviews: 4 },
+  { concept: "side", reviews: 4 },
+  { concept: "side", reviews: 4 },
+  { concept: "team", reviews: 4 },
+  { concept: "space", reviews: 4 },
+  { concept: "space", reviews: 4 },
+  { concept: "street", reviews: 4 },
+  { concept: "place", reviews: 4 },
+  { concept: "meat", reviews: 4 },
+  { concept: "Mexican", reviews: 4 },
+  { concept: "market", reviews: 4 },
+  { concept: "trip", reviews: 4 }
+];
